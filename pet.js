4ba7620737b0a96c705995556396b6ab314b15ab
Md. Nahid Hasan Bappi <nahidbappi13@gmail.com>
2024-10-17 18:18:05 +0600

    created a responsive navbar

---
+++ pet.js
@@ -0,0 +1,13 @@
+
+
+function toggleMenu() {
+    const menu = document.getElementById('mobile-menu');
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
+}
+
+
+
